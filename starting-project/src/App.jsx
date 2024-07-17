1ef0b9ee32f117a4cbf4c0a2badd4e71a0f77173
Gustavo Beretta Gonçalves <gustavoberettag@gmail.com>
2024-07-17 13:02:19 -0300

    Initial commit

---
+++ starting-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <main>
+      <Post author="Maximilian" body="React.js IS Awesome" />
+      <Post />
+      <Post />
+      <Post />
+    </main>
+  )
+}
+
+export default App;
